d34beb15368b553db2bcbf006c302a149293653e
Elena-Weber <crochetmiracles@gmail.com>
2021-05-26 16:59:50 -0400

    commit tree b980c615546fe41b993be12acedf36c5beda2e20

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 34;
+let num4 = 15;
 
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 5) + 1;
+let mod = num3 % num4;
+let max = Math.max(1, 3, 5, 18, 20);
 // Write your code here
